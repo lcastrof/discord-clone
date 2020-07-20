d7da52252ec157099d672e9f408cf59a1c0da388
Lucas de Castro <castro.lucas@engenharia.ufjf.br>
2020-07-20 15:25:51 -0300

    channel button

---
+++ src/components/ChannelButton/styles.ts
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+import { Hashtag } from 'styled-icons/heroicons-outline';
+import { PersonAdd, Settings } from 'styled-icons/material';
+
+export const Container = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+  cursor: pointer;
+  
+  padding: 5px 3px;
+  border-radius: 5px;
+
+  background-color: transparent;
+
+  transition: background-color 0.2s;
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
+
+  > div span {
+    margin-left: 5px;
+    color: var(--senary);
+  }
+
+  > div#side-icons svg {
+    display: none;
+  }
+
+  &:hover, &.active {
+    background-color: var(--quinary);
+
+    > div span {
+      color: var(--white);
+    }
+
+    > div#side-icons svg {
+      display: inline;
+    }
+  }
+`;
+
+export const HashTagIcon = styled(Hashtag)`
+  width: 20px;
+  height: 20px;
+
+  color: var(--symbol);
+`;
+
+export const InviteIcon = styled(PersonAdd)`
+  width: 18px;
+  height: 18px;
+
+  color: var(--symbol);
+  cursor: pointer;
+
+  transition: color 0.2s;
+
+  &:hover {
+    color: var(--white);
+  }
+`;
+
+export const SettingsIcon = styled(Settings)`
+  width: 18px;
+  height: 18px;
+
+  margin-left: 4px;
+
+  color: var(--symbol);
+  cursor: pointer;
+
+  transition: color 0.2s;
+  
+  &:hover {
+    color: var(--white);
+  }
+`;
